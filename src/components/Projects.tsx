a7c67b4fc817148aeb80cccc9183bf3f89619924
Shyamala9926 <shyamalajanamoni2002@gmail.com>
2025-05-24 21:27:35 +0530

    new changes in code

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -71,9 +71,7 @@ const Projects: React.FC = () => {
         whileInView={{ opacity: 1 }}
         transition={{ duration: 0.5 }}
       >
-        <p className='sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider'>
-          My work
-        </p>
+        
         <h2 className='text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]'>
           Projects.
         </h2>
